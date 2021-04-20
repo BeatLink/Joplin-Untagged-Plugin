e64fb2a08fcb6c3b2eb8d2f5c3ff3fd4cc8887e5
BeatLink <beatlink+git@simplelogin.co>
2021-04-19 20:40:07 -0500

    Migrate from ID based to Title based tag finding

--- src/index.ts
+++ src/index.ts
@@ -8,16 +8,13 @@
  * 	- Imports																													*
  *  - Plugin Registration																										*
  *  - Main Function																												*
- * 	- Sub Functions																												*
- *  	- Log Startup																											*
- * 		- Configure Settings																									*
- * 		- Tag Notes Without Tags																								*
- * 		- Untag Notes with Tags																									*
- * 	- Helper Functions																											*
- * 		- On Tag Settings Changed																								*
- * 		- Update Last Tags																										*
- * 		- Get or Create Tag																										*
- * 		- Get Tag																									            *
+ *  - Log Startup																												*
+ * 	- Configure Settings																										*
+ *	- Tag Title Changed Handler																									*
+ * 	- Tag Notes Without Tags																									*
+ * 	- Untag Notes with Tags																										*
+ * 	- Get or Create Tag																											*
+ * 	- Get Tag																													*																									            *
  *******************************************************************************************************************************/
 
 
@@ -25,11 +22,6 @@
 
 import joplin from 'api';
 import { SettingItemType } from 'api/types';
-
-// Global Constants -------------------------------------------------------------------------------------------------------------
-
-const untaggedTagID = "756e7461676765640000000000000000";
-const untaggedDefaultTagTitle = ">> untagged <<";
 
 // Plugin Registration ----------------------------------------------------------------------------------------------------------
 
@@ -60,19 +52,55 @@ async function logStartup(){
 
 async function configureSettings(){
 	//This function initializes all settings related
+
 	await joplin.settings.registerSection("untaggedSettings", {
 		label: "Untagged",
 		iconName: 'fa fa-tag',
 		name: "Untagged"
 	})
 
+	await joplin.settings.registerSetting("untaggedLastTagTitle", {
+		label: "The last tag name before settings change (used to delete old tag names)",
+		value: "",
+		type: SettingItemType.String,
+		public: false,
+		section: 'untaggedSettings',
+	})
+
+	await joplin.settings.registerSetting("untaggedTagTitle", {
+		label: "Notes without tags will be given this tag",
+		value: ">> untagged <<",
+		type: SettingItemType.String,
+		public: true,
+		section: 'untaggedSettings',
+	})
+
 	await joplin.settings.registerSetting("untaggedEnableUntagging", {
-		label: "Remove Untagged tag from notes if they have another tag",
+		label: "Remove tag from notes if they have another tag",
 		value: true,
 		type: SettingItemType.Bool,
 		public: true,
 		section: 'untaggedSettings',
 	})
+
+	const currentTagTitle = await joplin.settings.value("untaggedTagTitle");
+	await joplin.settings.setValue("untaggedLastTagTitle", currentTagTitle);
+	await joplin.settings.onChange(onTagTitleChanged);
+}
+
+// Tag Title Changed Handler ----------------------------------------------------------------------------------------------------
+
+async function onTagTitleChanged(event){
+	if (event.keys.includes("untaggedTagTitle")){
+		const oldTagName = await joplin.settings.value("untaggedLastTagTitle");
+		console.log (oldTagName)
+		const oldTag = await getTag(oldTagName)
+		if (oldTag) {
+			await joplin.data.delete(['tags', oldTag.id])
+		}
+		const currentTagTitle = await joplin.settings.value("untaggedTagTitle");
+		await joplin.settings.setValue("untaggedLastTagTitle", currentTagTitle);	
+	}
 }
 
 // tag Notes Without Tags -------------------------------------------------------------------------------------------------------
@@ -92,6 +120,7 @@ async function tagNotesWithoutTags(){
 }
 
 // Untag Notes With Tags --------------------------------------------------------------------------------------------------------
+
 async function untagNotesWithTags(){
 	//Checks notes tagged with "Untagged" that have been given another tag and removes the "Untagged" tag
 
@@ -108,35 +137,24 @@ async function untagNotesWithTags(){
 	}
 }
 
-
 // Get or Create Tag ------------------------------------------------------------------------------------------------------------
 
 async function getOrCreateTag() {
 	//Searches for and returns tag with matching name. If not found, a new tag is created
+	const tagTitle = await joplin.settings.value("untaggedTagTitle")
+	const existingTag = await getTag(tagTitle)
+	return existingTag ? existingTag : await joplin.data.post(['tags'], null, {title: tagTitle.toLowerCase()});
+}
+
+// Get Tag ----------------------------------------------------------------------------------------------------------------------
+
+async function getTag(tagName: string){
 	var allTags = (await joplin.data.get(['tags'])).items
 	for (var tag of allTags){
-        if(tag.id == untaggedTagID){ 
+        if(tag.title == tagName.toLowerCase()){ 
 			return tag;
 		}
 	}
-
-	// This while loop tries to create a new tag with a random name until a unique name is found, at which point the function returns
-	try {
-		var newTag = await joplin.data.post(['tags'], null, {id: untaggedTagID, title: untaggedDefaultTagTitle});
-		return newTag;
-	} catch (err) {
-		while (true){
-			var randomString = Math.random().toString().substr(2, 5)
-			var backuptitle = ">> " + randomString + ' ' + untaggedDefaultTagTitle + ' ' + randomString + " << ";
-			try {
-				var newTag = await joplin.data.post(['tags'], null, {id: untaggedTagID, title: backuptitle});
-				return newTag;
-			} catch {
-				continue;
-			}
-		}
-	}
 }
-
 
 // End of Code ------------------------------------------------------------------------------------------------------------------
